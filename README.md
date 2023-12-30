766e47ecb931c13bff921e072808a142f099139b
sayleephadte <109852878+sayleephadte@users.noreply.github.com>
2023-12-30 10:37:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Resume Builder Website
+
+Welcome to our Resume Builder Website! Our website allows users to easily create professional and personalized resumes.
+
+## Features
+- User-friendly interface for creating and editing resumes
+- Templates to choose from for different job industries
+- PDF export option for final resume
+- Dark mode for easy viewing
+- Image upload for profile picture
+
+### Built With
+- [React](https://reactjs.org/)
+- [Bootstrap](https://getbootstrap.com/)
+- [Chakra UI](https://chakra-ui.com/)
+- [React Router](https://reactrouter.com/)
+- [React Icons](https://react-icons.github.io/react-icons/)
+- [React HTML to PDF](https://www.npmjs.com/package/react-html-to-pdf)
+
+### Authors
+- [Saylee Phadte]
+
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import './App.css';
+import ResumeState from './Context/ResumeState';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Pages/About/About';
+
+function App() {
+  return (
+    <ResumeState>
+      <div className="App">
+        <Helmet>
+          <title>Resume Builder - Create Professional Resumes Online</title>
+          <meta name="description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
+          <meta name="keywords" content="resume builder, professional resumes, online resumes, resume templates" />
+          <meta name="author" content="Saylee Phadte" />
+          <meta property="og:title" content="Resume Builder - Create Professional Resumes Online" />
+          <meta property="og:description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
+          <meta property="og:type" content="website" />
+        </Helmet>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </ResumeState>
+  );
+}
+
+export default App;
